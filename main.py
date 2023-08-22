2159d161add888a554c6744732f7c4f3c6aa7829
mateopascual <mateojspascual@gmail.com>
2023-08-22 14:58:58 -0700

    build bag of words

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
+import random
+import numpy
 import nltk
-from nltk.stem.lancaster import  LancasterStemmer
 nltk.download('punkt')
 
 with open("Knowledge_Base.json") as json_data:
   data = json.load(json_data)
+
 
 words = []
 documents = []
@@ -20,26 +22,51 @@ for intent in data["intents"]:
     if intent["tag"] not in classes:
       classes.append(intent["tag"])
 
+from nltk.stem.lancaster import LancasterStemmer
 
-#cleaning data with stemmer
 stemmer = LancasterStemmer()
-words = [stemmer.stem(word.lower()) for word in words]
-words = sorted(list(set(words)))
+words_lowercase = [stemmer.stem(word.lower()) for word in words]
+words = sorted(list(set(words_lowercase)))
 
-#Bag of words
+#  Build bag of words for ML model
 
-traing_data = []
+
+
 empty_output = [0] * len(classes)
+
+
+
+training_data = []
 
 for document in documents:
   bag_of_words = []
+
   pattern_words = document[0]
   pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
 
   for word in words:
     bag_of_words.append(1) if word in pattern_words else bag_of_words.append(0)
 
-output_row = list(empty_output)
-output_row[classes.index(document[1])] = 1
-traing_data.append([bag_of_words,output_row])
+  output_row = list(empty_output)
+  output_row[classes.index(document[1])] = 1
+  training_data.append([bag_of_words, output_row])
 
+
+"""#  Split data for machine learning"""
+
+random.shuffle(training_data)
+training_numpy = numpy.array(training_data)
+
+print(training_numpy)
+
+print(type(training_numpy))
+
+train_X = list(training_numpy[:,0])
+
+print(train_X)
+
+print(len(train_X))
+
+train_y = list(training_numpy[:,1])
+
+print(train_y)
