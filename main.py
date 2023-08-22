c39c606ad20ed4d2fb544a427b14e5f8c93c8e43
mateopascual <mateojspascual@gmail.com>
2023-08-22 14:52:11 -0700

    build bag of words

--- main.py
+++ main.py
@@ -23,7 +23,23 @@ for intent in data["intents"]:
 
 #cleaning data with stemmer
 stemmer = LancasterStemmer()
-
 words = [stemmer.stem(word.lower()) for word in words]
+words = sorted(list(set(words)))
 
-print(words)
+#Bag of words
+
+traing_data = []
+empty_output = [0] * len(classes)
+
+for document in documents:
+  bag_of_words = []
+  pattern_words = document[0]
+  pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+
+  for word in words:
+    bag_of_words.append(1) if word in pattern_words else bag_of_words.append(0)
+
+output_row = list(empty_output)
+output_row[classes.index(document[1])] = 1
+traing_data.append([bag_of_words,output_row])
+
